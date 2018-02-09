e1604a72fdd7f9414b7add3c3c66d30b91ffbf69
Arturo Guzman <ing.arturo.guzman@gmail.com>
2018-02-08 20:41:37 -0500

    chore: add typings folder

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="settings">
-    <h1>This is settings page</h1>
+    <h1>Settings</h1>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'settings',
+  data() {
+    return {
+      form: {
+        apiAccessToken: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit() {}
+  }
+})
+</script>
