4e8539f11f6df54b0a33746e5ef75f05ee52cb51
Arturo Guzman <ing.arturo.guzman@gmail.com>
2018-02-10 03:27:17 -0500

    chore: add fontawesome icons

---
+++ src/components/RegularIcon.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import Vue from 'vue'
+import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
+import fontawesome from '@fortawesome/fontawesome'
+import { faSpinner } from '@fortawesome/fontawesome-free-solid'
+
+fontawesome.library.add(faSpinner)
+
+// https://github.com/FortAwesome/vue-fontawesome
+
+export default Vue.extend({
+  name: 'RegularIcon',
+  extends: FontAwesomeIcon,
+  props: {
+    icon: String
+  }
+})
+</script>
