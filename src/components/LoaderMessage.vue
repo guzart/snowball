fee3e1716d843943f07ff12df467a6bfd814bfe5
Arturo Guzman <ing.arturo.guzman@gmail.com>
2018-02-10 03:27:25 -0500

    chore: add loader message

---
+++ src/components/LoaderMessage.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>
+    <RegularIcon icon="spinner" spin pulse size="lg" />
+    &nbsp;&nbsp;
+    <slot />
+  </p>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import RegularIcon from '@/components/RegularIcon.vue'
+
+export default Vue.extend({
+  name: 'LoaderMessage',
+  components: {
+    RegularIcon
+  }
+})
+</script>
+
--- src/components/SetupWizard.vue
+++ src/components/SetupWizard.vue
@@ -2,13 +2,19 @@
   <div>
     <div>
       <h1>Choose your Budget</h1>
+      <LoaderMessage>Loading available budgets...</LoaderMessage>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import LoaderMessage from '@/components/LoaderMessage.vue'
+
 export default Vue.extend({
-  name: 'SetupWizard'
+  name: 'SetupWizard',
+  components: {
+    LoaderMessage
+  }
 })
 </script>
