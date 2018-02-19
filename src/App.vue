9083531c7489c94750e435eb9a2645e6fdba363d
Arturo Guzman <ing.arturo.guzman@gmail.com>
2018-02-19 00:03:02 -0500

    chore: remove nav links

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Dashboard</router-link> |
-      <router-link to="/settings">Settings</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
@@ -18,18 +14,5 @@
   text-align: center;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
